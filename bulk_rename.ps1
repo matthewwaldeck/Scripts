9282f3bcda5bf0407fedf5bb23111aeee2bd7183
Matt <waldeck.me@gmail.com>
2019-06-27 12:24:37 -0400

    Create bulk_rename.ps1

---
+++ bulk_rename.ps1
@@ -0,0 +1,7 @@
+﻿#Directory to be scanned.
+cd #DIRECTORY
+
+#This will pipe all the files in your current directory
+#into Rename-Item, and replace whatever is in the first
+#set of quotes withwhatever is in the second.
+Dir | Rename-Item –NewName { $_.name –replace “ “,”_” }
