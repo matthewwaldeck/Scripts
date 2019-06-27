095d4bf15d34213d9efb7393bfa974cce041f938
Matt <waldeck.me@gmail.com>
2019-06-27 15:44:34 -0400

    Update system_report.ps1

--- system_report.ps1
+++ system_report.ps1
@@ -1,23 +1,32 @@
 ﻿cls
+#The basics
+$date = Get-Date -Format g
+'Current User: ' + $env:UserName
+"Today's Date: " + $date
+
+''
+''
 #information about the System
-$system = Get-WMIObject -Class Win32_ComputerSystem
 'System'
+$system = Get-WMIObject -Class Win32_ComputerSystem
 'Manufacturer: ' + $system.Manufacturer
 'Model: ' + $system.Model
 'Name: ' + $system.Name
 'Domain: ' + $system.Domain
-'Memory: ' + $system.TotalPhysicalMemory
+'Memory: ' + [math]::Round($system.TotalPhysicalMemory / 1GB,1) + 'GB'
 
 
 ''
+''
 #BIOS information
-$bios = Get-WmiObject -Class Win32_BIOS
 'BIOS'
+$bios = Get-WmiObject -Class Win32_BIOS
 'Computer Name: ' + $bios.PSComputerName
 'Manufacturer: ' + $bios.Manufacturer
 'BIOS Version: ' + $bios.SMBIOSBIOSVersion
 
 
+''
 ''
 #CPU specifications
 'Processor'
@@ -34,7 +43,18 @@ foreach ($_ in $cpu) {
 ''
 #Information about Logical Drives (Includes mapped drives and I believe PSDrives)
 'Storage'
-Get-PSDrive | Where-Object {$_.Provider.Name -eq "FileSystem"}
+#Get-PSDrive | Where-Object {$_.Provider.Name -eq "FileSystem"}
+$storage = (Get-WmiObject -Class Win32_logicaldisk)
+foreach ($_ in $storage) {
+    'Label: ' + $_.DeviceID
+    if ($_.VolumeName -ne '') {
+       'Name: ' + $_.VolumeName
+    }
+    'Capacity: ' + [math]::Round($_.Size / 1GB,2) + 'GB'
+    'Used: ' + [math]::Round(($_.Size - $_.FreeSpace) / 1GB,2) + 'GB'
+    'Free: ' + [math]::Round($_.FreeSpace / 1GB,2) + 'GB'
+    ''
+}
 
 
 ''
@@ -42,7 +62,7 @@ Get-PSDrive | Where-Object {$_.Provider.Name -eq "FileSystem"}
 'Network'
 $network = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE)
 foreach ($_ in $network) {
-     'Name: ' + $_.Description
-     'IP Address: ' + $_.IPAddress
-     ''
+    'Name: ' + $_.Description
+    'IP Address: ' + $_.IPAddress
+    ''
 }
