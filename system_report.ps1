66da5a7fba0d95830b5fb822d9e6ab29235b8f5c
Matt <waldeck.me@gmail.com>
2019-06-27 15:09:19 -0400

    Update system_report.ps1

--- system_report.ps1
+++ system_report.ps1
@@ -10,23 +10,39 @@ $system = Get-WMIObject -Class Win32_ComputerSystem
 
 
 ''
-#Information about the BIOS
+#BIOS information
 $bios = Get-WmiObject -Class Win32_BIOS
 'BIOS'
 'Computer Name: ' + $bios.PSComputerName
-'Manufacturer:  ' + $bios.Manufacturer
-'BIOS Version:  ' + $bios.SMBIOSBIOSVersion
+'Manufacturer: ' + $bios.Manufacturer
+'BIOS Version: ' + $bios.SMBIOSBIOSVersion
+
 
 ''
-#Information about the CPU
-$cpu = Get-WMIObject -Class Win32_Processor
+#CPU specifications
 'Processor'
-'Number: ' + $cpu.DeviceID
-'Manufacturer: ' + $cpu.Manufacturer
-'Model: ' + $cpu.Name
-'Speed: ' + $cpu.MaxClockSpeed
+$network = ($cpu = Get-WMIObject -Class Win32_Processor)
+foreach ($_ in $cpu) {
+    'Number: ' + $cpu.DeviceID
+    'Manufacturer: ' + $cpu.Manufacturer
+    'Model: ' + $cpu.Name
+    'Speed: ' + $cpu.MaxClockSpeed
+    ''
+}
+
 
 ''
 #Information about Logical Drives (Includes mapped drives and I believe PSDrives)
 'Storage'
 Get-PSDrive | Where-Object {$_.Provider.Name -eq "FileSystem"}
+
+
+''
+#Network configuration details
+'Network'
+$network = (Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter IPEnabled=TRUE)
+foreach ($_ in $network) {
+     'Name: ' + $_.Description
+     'IP Address: ' + $_.IPAddress
+     ''
+}
