178f2c123ecac5b4fcaf077ddf447eec3f32e9eb
Matt <waldeck.me@gmail.com>
2019-07-04 16:17:45 -0400

    Update ad_admin.ps1

--- Active Directory/ad_admin.ps1
+++ Active Directory/ad_admin.ps1
@@ -7,26 +7,6 @@
     Version:    v1.3.2
 #>
 
-<# SETUP - PLEASE READ BEFORE RUNNING
-    Before this script can be run, you must set your execution policy to Remote Signed.
-        - Run this command (no quotes) "Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine"
-        - You must respond yes (y) or all (a) in order to use this script.
-        - Place this file wherever you like.
-        - Run and login whenever you would like to use AD with your admin account.
-        - Close the CMD window that appears with AD if you wish, it's just used to open the application.
-
-    Once you have done this, set Windows to run .ps1 files with PowerShell by default
-        - Right-click on the script
-        - Select "Open with..."
-        - Select "Look for another app on this PC"
-        - Navigate to C:\WINDOWS\System32\Windows PowerShell\v1.0\
-        - Select PowerShell.exe
-        - Check "Always run with..."
-    
-    Once complete, modify the below code to generate your admin login from your normal account login.
-    Then you can plug in your domain and you should be good to go!
-#>
-
 # Seperate first and last name.
 $login = $env:USERNAME 
 $pos = $login.IndexOf(".")
