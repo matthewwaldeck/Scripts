1c867240923eda486356aab55134717ef6c48ace
Matt <waldeck.me@gmail.com>
2019-07-08 10:12:19 -0400

    Delete disk_usage.ps1

--- Reports/disk_usage.ps1
+++
@@ -1,9 +0,0 @@
-﻿$size = 0
-
-foreach ($file in (get-childitem -Path C:\Users\$env:USERNAME -Recurse -ErrorAction SilentlyContinue)) {
-    $size += $file.length
-}
-
-$size = [math]::round($size/1GB,2)
-"$env:USERNAME - " + $size + 'GB'
-Pause
