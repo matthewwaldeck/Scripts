61eb588fbfc7861d0761d3b456264cf89960f2ab
Matt <waldeck.me@gmail.com>
2019-07-10 14:22:22 -0400

    Update get-aduser.ps1
    Added console feedback.

--- Active Directory/get-aduser.ps1
+++ Active Directory/get-aduser.ps1
@@ -5,10 +5,12 @@
     Language:   PowerShell
     Purpose:    Lists all users in the current domain.
     Last Edit:  07-09-2019
-    Version:    v1.0.0
+    Version:    v1.1.0
 #>
 
 # Allow AD commands
 Import-Module ActiveDirectory
 
+Write-Output "Generating CSV..."
 Get-ADUser -Filter * | Select-Object -Property Name, ObjectClass, Enabled, UserPrincipalName, DistinguishedName | Export-Csv C:\adusers_report.csv
+Write-Output "Done!"
