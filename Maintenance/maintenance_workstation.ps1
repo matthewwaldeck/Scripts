432a7eb13c091e3c1faf6d5b3461ad8e595b4b8c
Matt <waldeck.me@gmail.com>
2019-07-25 22:41:10 -0400

    +logPath
    Switched to using a predefined variable for the log path throughout the script.

--- Maintenance/maintenance_workstation.ps1
+++ Maintenance/maintenance_workstation.ps1
@@ -5,7 +5,7 @@
     Language:   PowerShell
     Purpose:    Will perform routine maintenance tasks for workstations.
     Last Edit:  07-25-2019
-    Version:    v1.2.1
+    Version:    v1.2.2
 
     Tasks:
       -Clean up temp files
@@ -24,6 +24,7 @@
 #Get pre-maintenance used disk space
 $TempDisk = Get-WmiObject -Class win32_logicaldisk
 $OldDisk = ([Math]::Round($TempDisk.Capacity /1GB,2)) - ([Math]::Round($TempDisk.FreeSpace /1GB,2))
+$logPath = "C:\Users\$env:UserName\Desvktop\maintenance.log"
 
 #Functions
 function Test-Administrator {
@@ -41,13 +42,13 @@ function RunCheckDisk {
   "Running CheckDisk..."
   $Disks = Get-WmiObject -Class win32_logicaldisk | Where-Object {$PSItem.DriveType -eq 3} | Select-Object Name -ExpandProperty Name
   foreach ($Disk in $Disks) {
-      "$(Get-TimeStamp) - Check disk on $Disk started." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+      "$(Get-TimeStamp) - Check disk on $Disk started." | Add-Content -Path $logPath
       $Output = chkdsk $Disk | Select-String 'Windows has scanned the file system and found no problems.'
       if ($Output.ToString() -eq 'Windows has scanned the file system and found no problems.') {
-          "$(Get-TimeStamp) - Check Disk completed on $Disk drive successfully." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+          "$(Get-TimeStamp) - Check Disk completed on $Disk drive successfully." | Add-Content -Path $logPath
       }
       else {
-          "$(Get-TimeStamp) - Check Disk has been scheduled to run on reboot on $Disk." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+          "$(Get-TimeStamp) - Check Disk has been scheduled to run on reboot on $Disk." | Add-Content -Path $logPath
           Write-Output 'y' | chkdsk $Disk /f
       }
   }
@@ -55,25 +56,25 @@ function RunCheckDisk {
 
 function OptimizeDrives {
   "Optimizing and Defragging drives..."
-  "$(Get-TimeStamp) - Beginning defrag on disks..." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+  "$(Get-TimeStamp) - Beginning defrag on disks..." | Add-Content -Path $logPath
   $Disks=Get-WmiObject -Class win32_logicaldisk | Where-Object {$PSItem.DriveType -eq 3} | Select-Object Name -ExpandProperty Name
   ForEach ($Disk in $Disks) {
-      "$(Get-TimeStamp) - Beginning defrag on $Disk." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+      "$(Get-TimeStamp) - Beginning defrag on $Disk." | Add-Content -Path $logPath
       defrag $Disk /O | Out-Null
-      "$(Get-TimeStamp) - Completed defrag on $Disk." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+      "$(Get-TimeStamp) - Completed defrag on $Disk." | Add-Content -Path $logPath
   }
 }
 
 function RunSFCScan {
   # Run sfcscan /scannow
   "Running SFC scan..."
-  "$(Get-TimeStamp) - Started SFC scan." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+  "$(Get-TimeStamp) - Started SFC scan." | Add-Content -Path $logPath
   sfc /ScanNow | Out-Null
   if ($LASTEXITCODE -eq 1) {
-      "$(Get-TimeStamp) - Failed to run SFC scan." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+      "$(Get-TimeStamp) - Failed to run SFC scan." | Add-Content -Path $logPath
   }
   else {
-    "$(Get-TimeStamp) - Completed SFC scan." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log}
+    "$(Get-TimeStamp) - Completed SFC scan." | Add-Content -Path $logPath}
 }
 
 function CleanupTemp {
@@ -81,28 +82,28 @@ function CleanupTemp {
   "Cleaning up temp folder (if present)..."
   $TempDir = "$env:SystemDrive\Temp\"
   if (Test-Path $TempDir) {
-    "$(Get-TimeStamp) - Cleaning up temp files in $TempDir..." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+    "$(Get-TimeStamp) - Cleaning up temp files in $TempDir..." | Add-Content -Path $logPath
     $GetTempDir = Get-ChildItem $TempDir -Recurse | Where-Object {$PSItem.LastWriteTime -lt (Get-Date).AddYears(-1)}
     foreach ($Item in $GetTempDir) {
         try {
-            "$(Get-TimeStamp) - Removing $Item.Name from $TempDir." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+            "$(Get-TimeStamp) - Removing $Item.Name from $TempDir." | Add-Content -Path $logPath
             Remove-Item $Item.FullName -Force -ErrorAction Stop
         }
         catch {
-            "$(Get-TimeStamp) - Failed to remove $Item" | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+            "$(Get-TimeStamp) - Failed to remove $Item" | Add-Content -Path $logPath
         }
     }
     try {
         $GetTempDir = Get-ChildItem $TempDir -Recurse | Where-Object {$PSItem.LastWriteTime -lt (Get-Date).AddYears(-1)}; $GetTempDir = $GetTempDir.Count
     }
     catch {
-        "$(Get-TimeStamp) - No files in $TempDir meet criteria." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+        "$(Get-TimeStamp) - No files in $TempDir meet criteria." | Add-Content -Path $logPath
     }
     if ($GetTempDir -gt 0) {
-        "$(Get-TimeStamp) - There are $GetTempDir files remaining in $TempDir." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+        "$(Get-TimeStamp) - There are $GetTempDir files remaining in $TempDir." | Add-Content -Path $logPath
     }
   } else {
-    "$(Get-TimeStamp) - There is no temp file present!" | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+    "$(Get-TimeStamp) - There is no temp file present!" | Add-Content -Path $logPath
   }
   Clear-Variable TempDir
 }
@@ -112,13 +113,13 @@ function CleanupWinTemp {
   "Cleaning up Windows temp folder..."
   $TempDir = $env:windir + '\Temp\'
   try {
-      "$(Get-TimeStamp) - Cleaning up temp files in $TempDir..." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+      "$(Get-TimeStamp) - Cleaning up temp files in $TempDir..." | Add-Content -Path $logPath
       Remove-Item $TempDir\* -Recurse -Force -ErrorAction Stop
   }
   catch {
-      "$(Get-TimeStamp) - Failed to clean all temp files in $TempDir." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+      "$(Get-TimeStamp) - Failed to clean all temp files in $TempDir." | Add-Content -Path $logPath
       $Remaining = Get-ChildItem $TempDir\* -Recurse; $Remaining = $Remaining.Count
-      "$(Get-TimeStamp) - There are $Remaining items present in $TempDir after cleaning." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+      "$(Get-TimeStamp) - There are $Remaining items present in $TempDir after cleaning." | Add-Content -Path $logPath
   }
   Clear-Variable TempDir
 }
@@ -131,12 +132,12 @@ function CleanupAppDataTemp {
       try {
           $Dir = "$Dir\AppData\Local\Temp\"
           Get-ChildItem $Dir -Recurse | Select-Object FullName, LastWriteTime | Where-Object {$PSItem.LastWriteTime -lt (Get-Date).AddDays(-7)} | Select-Object FullName -ExpandProperty FullName | Remove-Item -Exclude *ssh* -Recurse -ErrorAction SilentlyContinue
-          "$(Get-TimeStamp) - Cleaning up temp files in $Dir..." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+          "$(Get-TimeStamp) - Cleaning up temp files in $Dir..." | Add-Content -Path $logPath
       }
       catch {
-          "$(Get-TimeStamp) - Failed to clean all temp files in $Dir." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+          "$(Get-TimeStamp) - Failed to clean all temp files in $Dir." | Add-Content -Path $logPath
           $Remaining = Get-ChildItem $Dir\* -Recurse; $Remaining = $Remaining.Count
-          "$(Get-TimeStamp) - There are $Remaining items present in $Dir after cleaning." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+          "$(Get-TimeStamp) - There are $Remaining items present in $Dir after cleaning." | Add-Content -Path $logPath
       }
   }
   Clear-Variable TempDir
@@ -149,11 +150,11 @@ function EmptyRecycleBin {
     $NewShell = New-Object -ComObject Shell.Application -ErrorAction Stop
     $GetRecycleBin = $NewShell.NameSpace(0xA)
     $GetRecycleBin.Items() | ForEach-Object {Remove-Item $PSItem.Path -Recurse -Force -ErrorAction Stop} -ErrorAction Stop
-    "$(Get-TimeStamp) - Emptied recycle bin." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+    "$(Get-TimeStamp) - Emptied recycle bin." | Add-Content -Path $logPath
   }
   catch {
-    "$(Get-TimeStamp) - ERROR: $($PSItem.Exception)" | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
-    "$(Get-TimeStamp) - Failed to empty recycle bin." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+    "$(Get-TimeStamp) - ERROR: $($PSItem.Exception)" | Add-Content -Path $logPath
+    "$(Get-TimeStamp) - Failed to empty recycle bin." | Add-Content -Path $logPath
   }
 }
 
@@ -161,18 +162,18 @@ function ShutdownComputer {
   #Should be the last thing to run
   $Time = (Get-Date).ToString('yyyy-MM-dd hh:mm:ss tt')
   $ShutdownTime = (Get-Date).AddMinutes(30).ToString('yyyy-MM-dd hh:mm:ss tt')
-  "$(Get-TimeStamp) - The last shutdown was $LastBootUp day(s) ago." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
-  "$(Get-TimeStamp) - Maintenance finished at $Time. Shutting down $env:COMPUTERNAME at $ShutdownTime." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+  "$(Get-TimeStamp) - The last shutdown was $LastBootUp day(s) ago." | Add-Content -Path $logPath
+  "$(Get-TimeStamp) - Maintenance finished at $Time. Shutting down $env:COMPUTERNAME at $ShutdownTime." | Add-Content -Path $logPath
   shutdown /s /t 1800
 }
 
 
 #Script
-"$(Get-TimeStamp) - Maintenance started by $env:UserName" | Set-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+"$(Get-TimeStamp) - Maintenance started by $env:UserName" | Set-Content -Path $logPath
 
 if (!(Test-Administrator)) {
-  "$(Get-TimeStamp) - Script must be run as administrator. Exiting script." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
-  "$(Get-TimeStamp) - Maintenance failed!" | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+  "$(Get-TimeStamp) - Script must be run as administrator. Exiting script." | Add-Content -Path $logPath
+  "$(Get-TimeStamp) - Maintenance failed!" | Add-Content -Path $logPath
   exit
 }
 
@@ -195,12 +196,12 @@ RunSFCScan
 $TempDisk = Get-WmiObject -Class win32_logicaldisk
 $NewDisk = ([Math]::Round($TempDisk.Capacity /1GB,2)) - ([Math]::Round($TempDisk.FreeSpace /1GB,2))
 
-"You have freed up " + ($OldDisk-$NewDisk) + " GB on your C: drive." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
-"$(Get-TimeStamp) - Maintenance completed!" | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+"You have freed up " + ($OldDisk-$NewDisk) + " GB on your C: drive." | Add-Content -Path $logPath
+"$(Get-TimeStamp) - Maintenance completed!" | Add-Content -Path $logPath
 "$(Get-TimeStamp) - Shutdown = $shutdown"
 if ($shutdown -eq 1) {
-  "$(Get-TimeStamp) - Shutting down workstation in 30 minutes." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+  "$(Get-TimeStamp) - Shutting down workstation in 30 minutes." | Add-Content -Path $logPath
   ShutdownComputer
 } else {
-  "$(Get-TimeStamp) - Workstation will not shut down." | Add-Content -Path C:\Users\$env:UserName\Desktop\maintenance.log
+  "$(Get-TimeStamp) - Workstation will not shut down." | Add-Content -Path $logPath
 }
