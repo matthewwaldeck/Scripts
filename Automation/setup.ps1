fa481d80d94425379c13fae456e0495551e73efb
Matt <waldeck.me@gmail.com>
2019-07-26 14:12:41 -0400

    Added main functionality
    Downloads, installs, and cleans up after Chrome, Spotify and VSCode currently. More to come. May eb renamed at some point. Function Setup is currently disabled for testing.

--- Automation/setup.ps1
+++ Automation/setup.ps1
@@ -5,5 +5,87 @@
     Language:   PowerShell
     Purpose:    Downloads and installs basic software on a new computer.
     Last Edit:  07-26-2019
-    Version:    v0.1.0
+    Version:    v0.1.0 (BETA)
+
+    TASKS:
+    -Log all tasks
+    -Download, install, and clean up software
+    -Set sleep and power options (Coming Soon)
 #>
+
+$logPath = "C:\Users\$env:UserName\Desktop\setup_$(get-date -f yyyy-MM-dd-HHmm).log" #Log file destination
+
+# FUNCTIONS #
+function Get-TimeStamp {
+    return "[{0:MM/dd/yy} at {0:HH:mm:ss}]" -f (Get-Date)
+  }
+
+function tempCheck {
+    #Check for existence of temp file
+    $tempCheck = Test-Path C:\temp
+    if ($tempCheck -eq $False) {
+        New-Item -Path "C:\temp\" -ItemType Directory | Out-Null
+        "Created C:\temp" | Add-Content -Path $logPath
+        Write-Host "Created temp file."
+    }
+}
+
+function download {
+    #Google Chrome
+    Write-Host "Downloading Google Chrome..."
+    (New-Object System.Net.WebClient).DownloadFile('http://dl.google.com/chrome/install/375.126/chrome_installer.exe', 'C:\temp\GoogleChrome.exe')
+
+    #Spotify
+    Write-Host "Downloading Spotify..."
+    (New-Object System.Net.WebClient).DownloadFile('https://download.scdn.co/SpotifySetup.exe', 'C:\temp\Spotify.exe')
+
+    #Spotify
+    Write-Host "Downloading Visual Studio Code..."
+    (New-Object System.Net.WebClient).DownloadFile('https://aka.ms/win32-x64-user-stable', 'C:\temp\VS_Code.exe')
+    
+}
+
+function setup {
+    try {
+        Write-Host "Installing Google Chrome..."
+        C:\temp\GoogleChrome.exe /silent /install
+        Write-Host "Google Chrome has been installed!"
+        "$(Get-TimeStamp) - Successfully installed Google Chrome!" | Add-Content -Path $logPath
+    } Catch {
+        "Failed to install Google Chrome!" | Add-Content -Path $logPath
+        $_ | Add-Content -Path $logPath
+    }
+
+    try {
+        Write-Host "Installing Spotify..."
+        C:\temp\Spotify.exe /silent /install
+        Write-Host "Spotify has been installed!"
+        "$(Get-TimeStamp) - Successfully installed Spotify!" | Add-Content -Path $logPath
+    } Catch {
+        "Failed to install Spotify!" | Add-Content -Path $logPath
+        $_ | Add-Content -Path $logPath
+    }
+
+    try {
+        Write-Host "Installing Visual Studio Code..."
+        C:\temp\VS_Code.exe /silent /install
+        Write-Host "Visual Studio Code has been installed!"
+        "$(Get-TimeStamp) - Successfully installed Visual Studio Code!" | Add-Content -Path $logPath
+    } Catch {
+        "Failed to install Visual Studio Code!" | Add-Content -Path $logPath
+        $_ | Add-Content -Path $logPath
+    }
+}
+
+function cleanup {
+    Write-Host "Cleaning up..."
+    Remove-Item C:\temp -rec
+}
+
+# SCRIPT #
+"Setup started at $(Get-TimeStamp) by $env:UserName" | Add-Content -Path $logPath
+tempCheck
+download
+#setup
+cleanup
+"Setup completed at $(Get-TimeStamp)" | Add-Content -Path $logPath
