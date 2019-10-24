d6309b7a5dd8c64e16faa8c996be4a0bec082787
Matt <waldeck.me@gmail.com>
2019-10-24 15:12:52 -0400

    Update good_morning.ps1
    Made Chrome launch more reliable, and commented out Teams link. It cuases errors currently.

--- Other/good_morning.ps1
+++ Other/good_morning.ps1
@@ -5,7 +5,7 @@
     Purpose:    Launches all my usual apps in the morning, but only during work hours. This ensures that if my laptop is turned on outside of work hours
                 it doesn't instantly load my entire suite of apps. This saves battery life and improves performance when working away from my desk.
     Last Edit:  10-24-2019
-    Version:    v2.0.1
+    Version:    v2.1.0
 
     NOTES:
     -If you are going to use this script, please remember to disable "Launch on Startup" settings in each app.
@@ -22,10 +22,10 @@ $dayEnd = "17" #End of work day. Must be in 24h format
 # FUNCTIONS
 function launchApps {
     Write-Host "Launching Apps..."
-    start-process "chrome.exe" "$intranetURL", "$helpdeskURL" #Chrome with The Daily and helpdesk open
+    start-process 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' "$intranetURL", "$helpdeskURL" #Chrome with intranet site and helpdesk open
     Start-Process 'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE' #Outlook
     Start-Process 'C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe' #Skype
-    Start-Process "C:\ProgramData\$env:USERNAME\Microsoft\Teams\Update.exe" #Teams
+    # !!!NOT FUNCTIONAL!!! Start-Process "C:\ProgramData\$env:USERNAME \Microsoft\Teams\Update.exe" #Teams
     Start-Process 'C:\Program Files (x86)\TeamViewer\TeamViewer.exe' #TeamViewer
     Start-Process 'C:\Program Files (x86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe' #RDP
     Write-Host "Done."
