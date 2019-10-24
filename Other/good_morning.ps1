ce14e94df20e8c3ca63504c06331000bc4a7af9c
Matt <waldeck.me@gmail.com>
2019-10-24 12:50:22 -0400

    Prep for version 2.0
    Cleaning up a few things in preperation for version 2.0.

--- Other/good_morning.ps1
+++ Other/good_morning.ps1
@@ -3,18 +3,24 @@
     Date:       07-12-2019
     Language:   PowerShell
     Purpose:    Launches all my usual apps in the morning, with the exceptin of AD and Citrix AppCenter (Require admin logins).
-    Last Edit:  07-15-2019
-    Version:    v1.1.0
+    Last Edit:  10-24-2019
+    Version:    v1.2.0
 
-    To-Do
-    -Add time/date condition for starting apps
-    -Have the script run whenever the computer is started
-    -Add my AD and Citrix Receiver scripts to this, including a password prompt
+    NOTES:
+    -If you are going to use this script, please remember to disable "Launch on Startup" settings in each app.
+    -Please also remember to update the work hours criteria with your normal work hours.
 #>
 
+# VARIABLES
+$helpdeskURL = "http://phl-helpdesk:8080/WOListView.do"
+#$dayStart = ""
+#$dayEnd = ""
+
+#This will be used to figure out if within working hours.
+#Get-Date -Format "HH:mm"
+
 # Google Chrome
-#Start-Process 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
-start-process "chrome.exe" "http://phl-helpdesk:8080/WOListView.do", "https://community.spiceworks.com/", "https://www.theverge.com", "https://9to5mac.com/"
+start-process "chrome.exe" "$helpdeskURL"
 
 # Microsoft Outlook
 Start-Process 'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE'
@@ -30,9 +36,3 @@ Start-Process 'C:\Program Files (x86)\TeamViewer\TeamViewer.exe'
 
 # Remote Desktop Connection Manager
 Start-Process 'C:\Program Files (x86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe'
-
-# GitHub Desktop
-Start-Process "C:\ProgramData\$env:USERNAME\GitHubDesktop\GitHubDesktop.exe"
-
-# Visual Studio Code
-Start-Process "C:\Users\$env:USERNAME\AppData\Local\Programs\Microsoft VS Code\Code.exe"
