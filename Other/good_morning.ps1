de413e46fcd34c1bc5b82410542c1cb4f36e15b1
Matt <waldeck.me@gmail.com>
2019-10-24 14:15:06 -0400

    Update good_morning.ps1
    Update description, added the Intranet site to Chrome's arguments.

--- Other/good_morning.ps1
+++ Other/good_morning.ps1
@@ -2,31 +2,32 @@
     Developer:  Matt Waldeck
     Date:       07-12-2019
     Language:   PowerShell
-    Purpose:    Launches all my usual apps in the morning, with the exceptin of AD and Citrix AppCenter (Require admin logins).
+    Purpose:    Launches all my usual apps in the morning, but only during work hours. This ensures that if my laptop is turned on outside of work hours
+                it doesn't instantly load my entire suite of apps. This saves battery life and improves performance when working away from my desk.
     Last Edit:  10-24-2019
-    Version:    v2.0.0
+    Version:    v2.0.1
 
     NOTES:
     -If you are going to use this script, please remember to disable "Launch on Startup" settings in each app.
     -Please also remember to update the work hours criteria with your normal work hours.
-    -You can add more websites to auto-open with Chrome in a comma-seperated list
-        -ie. start-process "chrome.exe" "$helpdeskURL", "https://news.google.com", https://www.theverge.com
+    -You can add more websites to auto-open with Chrome by adding to the comma seperated list.
 #>
 
-# VARIABLES
+# VARIABLES & URLs
+$intranetURL = "http://daily.cowangroup.ca/EN/Pages/default.aspx"
 $helpdeskURL = "http://phl-helpdesk:8080/WOListView.do"
-$dayStart = "09" #Must be in 24h format
-$dayEnd = "17" #Must be in 24h format
+$dayStart = "09" #Start of work day. Must be in 24h format
+$dayEnd = "17" #End of work day. Must be in 24h format
 
 # SCRIPT
 $time = Get-Date -Format "HH"
-if ($time -eq $dayStart -and $time -lt $dayend) {
+if (($time -ge $dayStart) -and ($time -lt $dayEnd)) {
     launchApps
 }
 
 # FUNCTIONS
 function launchApps {
-    start-process "chrome.exe" "$helpdeskURL" #Chrome with helpdesk open
+    start-process "chrome.exe" "$intranetURL", "$helpdeskURL" #Chrome with The Daily and helpdesk open
     Start-Process 'C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE' #Outlook
     Start-Process 'C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe' #Skype
     Start-Process "C:\ProgramData\$env:USERNAME\Microsoft\Teams\Update.exe" #Teams
