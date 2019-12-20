c645017167ca23f7ebabe6af13ebdc2c857c183d
Matt <waldeck.me@gmail.com>
2019-12-20 15:53:10 -0500

    Version 1.0.0 Release
    -Made reboot optional.
    -Code cleanup.
    -Optional defrag upon completion, as some PCs are severely fragmented from the factory.
    -Fixed bugs in logging system.
    -Log format updates.
    -Added more documentation throguhout code.

--- Automation/setup.ps1
+++ Automation/setup.ps1
@@ -4,28 +4,26 @@
     Date:       07-26-2018
     Language:   PowerShell
     Purpose:    Automates the basic setup process for a new Windows PC.
-    Last Edit:  12-19-2019
-    Version:    0.3.1
+    Last Edit:  12-20-2019
+    Version:    1.0.0
 
     TASKS:
-    -Create log
+    -Create log file
     -Rename the computer
     -Ensure temp file exists
-    -Download and install software
-        -Google Chrome
-        -Spotify
+    -Download and install Chrome and Spotify
     -Clean up temp file
+    -Optimize all attached drives
+    -Reboot the computer
 
     TO-DO:
-    -Programs to add
-        -Adobe Reader
     -Power modes
-    -Disk cleanup + defrag
-    -Remove junk from taskbar & start menu
+    -Clean up taskbar & start menu
     -Set defaults
 #>
 
 $logPath = "C:\Users\$env:UserName\Desktop\setup_$(get-date -f yyyy-MM-dd-HHmm).log" #Log file destination
+
 
 # FUNCTIONS #
 function Get-TimeStamp {
@@ -35,7 +33,8 @@ function Get-TimeStamp {
 function rename {
     $comp_name =  Read-Host "Enter desired computer name..."
     Rename-Computer -NewName $comp_name
-    Write-Host "Computer name set to $comp_name." | Add-Content -Path $logPath
+    Write-Host "Computer name set to $comp_name."
+    "$(Get-TimeStamp) - Computer name set to $comp_name." | Add-Content -Path $logPath
 }
 
 function tempCheck {
@@ -43,7 +42,7 @@ function tempCheck {
     $tempCheck = Test-Path C:\temp
     if ($tempCheck -eq $False) {
         New-Item -Path "C:\temp\" -ItemType Directory | Out-Null
-        "Created C:\temp" | Add-Content -Path $logPath
+        "$(Get-TimeStamp) - Created C:\temp" | Add-Content -Path $logPath
         Write-Host "Created temp file."
     }
 }
@@ -58,7 +57,7 @@ function download {
         Write-Host ''
     } Catch {
         Write-Host "Google Chrome failed to download!"
-        "Google Chrome failed to download!" | Add-Content -Path $logPath
+        "$(Get-TimeStamp) - Google Chrome failed to download!" | Add-Content -Path $logPath
         $_ | Add-Content -Path $logPath
         '' | Add-Content -Path $logPath
     }
@@ -72,7 +71,7 @@ function download {
         Write-Host ''
     } Catch {
         Write-Host "Spotify failed to download!"
-        "Spotify failed to download!" | Add-Content -Path $logPath
+        "$(Get-TimeStamp) - Spotify failed to download!" | Add-Content -Path $logPath
         $_ | Add-Content -Path $logPath
         '' | Add-Content -Path $logPath
     }
@@ -85,7 +84,8 @@ function install {
         Write-Host "Google Chrome has been installed!"
         "$(Get-TimeStamp) - Successfully installed Google Chrome!" | Add-Content -Path $logPath
     } Catch {
-        "Failed to install Google Chrome!" | Add-Content -Path $logPath
+        Write-Host "Failed to install Google Chrome!"
+        "$(Get-TimeStamp) - Failed to install Google Chrome!" | Add-Content -Path $logPath
         $_ | Add-Content -Path $logPath
         '' | Add-Content -Path $logPath
     }
@@ -96,7 +96,8 @@ function install {
         Write-Host "Spotify has been installed!"
         "$(Get-TimeStamp) - Successfully installed Spotify!" | Add-Content -Path $logPath
     } Catch {
-        "Failed to install Spotify!" | Add-Content -Path $logPath
+        Write-Host "Failed to install Spotify!"
+        "$(Get-TimeStamp) - Failed to install Spotify!" | Add-Content -Path $logPath
         $_ | Add-Content -Path $logPath
         '' | Add-Content -Path $logPath
     }
@@ -106,27 +107,61 @@ function cleanup {
     #Deleted temp files.
     Write-Host "Cleaning up..."
     Remove-Item C:\temp -rec
-    "Removed temp files." | Add-Content -Path $logPath
+    "$(Get-TimeStamp) - Removed temp files." | Add-Content -Path $logPath
+}
+
+function optimize {
+    if ($optimize -eq "y"){
+        "Optimizing and Defragging drives..."
+        "$(Get-TimeStamp) - Beginning defrag on disks..." | Add-Content -Path $logPath
+        $Disks=Get-WmiObject -Class win32_logicaldisk | Where-Object {$PSItem.DriveType -eq 3} | Select-Object Name -ExpandProperty Name
+        ForEach ($Disk in $Disks) {
+            "$(Get-TimeStamp) - Beginning defrag on $Disk." | Add-Content -Path $logPath
+            defrag $Disk /O | Out-Null
+            "$(Get-TimeStamp) - Completed defrag on $Disk." | Add-Content -Path $logPath
+        }
+    }
+}
+
+function fin {
+    '' | Add-Content -Path $logPath
+    "Setup completed at $(Get-TimeStamp)" | Add-Content -Path $logPath
+    Write-Host ''
+    if ($reboot -eq "y") {
+        Write-Host "Setup complete! Your PC will now restart..."
+        "$(Get-TimeStamp) - Restarting computer." | Add-Content -Path $logPath
+        Start-Sleep -Seconds 5
+        Restart-Computer
+    } else {
+        Write-Host "Setup complete!"
+        Start-Sleep -Seconds 5
+        exit
+    }
 }
 
 # SCRIPT #
 "Setup started at $(Get-TimeStamp) by $env:UserName" | Set-Content -Path $logPath
 '' | Add-Content -Path $logPath
-$setup = Read-Host "Would you like to install some basic programs such as Chrome and Spotify? (y/n)"
 
-rename #Rename the computer as per the user's choice.
-tempCheck #Check for existence of a temop folder. If not present, will be created.
-if ($setup -eq "y") { #Download and install some basic programs.
-    Write-Host "Beginning installation of programs..." | Add-Content -Path $logPath
+#Set flags before beginning.
+$setup = Read-Host "Would you like to install Google Chrome and Spotify? (y/n)"
+$optimize = Read-Host "Would you like to defrag any attached drives? (y/n)"
+$reboot = Read-Host "Would you like to reboot your PC upon completion? (y/n)"
+
+rename #Rename the PC, depenting on user input.
+tempCheck #Ensure temp folder exists.
+
+#Download and install some basic programs.
+if ($setup -eq "y") {
+    Write-Host "Beginning installation of programs..."
+    "$(Get-TimeStamp) - Beginning installation of programs..." | Add-Content -Path $logPath
     download
     install
 } else {
-    Write-Host "Programs will not be installed." | Add-Content -Path $logPath
+    Write-Host "Programs will not be installed."
+    "$(Get-TimeStamp) - Programs will not be installed." | Add-Content -Path $logPath
 }
-cleanup #Do some housekeeping.
 
-'' | Add-Content -Path $logPath
-"Setup completed at $(Get-TimeStamp)" | Add-Content -Path $logPath
-Write-Host ''
-Write-Host "Setup complete - Your PC will now restart."
-Restart-Computer
+cleanup #Clean up the temp folder.
+optimize #Optimize all drives, if requested.
+fin #Reboot the computer or close session, depending on flag.
