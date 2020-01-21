0bc03aafd1f794e83d2503125ac5e4d5508a368d
Matt <waldeck.me@gmail.com>
2020-01-21 09:53:04 -0500

    Update good_morning.ps1
    Cut out all the fluff, and rearranged some things.

--- Automation/good_morning.ps1
+++ Automation/good_morning.ps1
@@ -2,46 +2,25 @@
     Developer:  Matt Waldeck
     Date:       07-12-2019
     Language:   PowerShell
-    Purpose:    Launches all my usual apps in the morning, but only during work hours. This saves battery life and improves performance when working
-                from home by not auto-starting everything on my laptop.
-    Last Edit:  11-19-2019
-    Version:    v2.3.2
-
-    SETUP
-    -Create a task in Task Schduler to be run when you log into your account
-    -Set up an action with the following settings
-        -Program = powershell.exe
-        -Arguments = .\good_morning.ps1
-        -Start in = path to wherever you put this script (i.e. C:\Scripts)
-    -Under the Conditions tab, disable AC power condition
-
-    NOTES
-    -If you are going to use this script, please remember to disable "Launch on Startup" settings in each app.
-    -You can add more websites to auto-open with Chrome by adding to the comma seperated list.
-    -$dayStart should be the hour before you start work, in case you come in early.
-    -Please feel free to let me know if there are any apps you would like added and I will happily add them.
+    Purpose:    Launches all my usual apps in the morning. Place shortcut on desktop and run once logged in.
+    Last Edit:  01-21-2020
+    Version:    v3.0.0
 #>
 
 
 # VARIABLES & URLS
 #$intranetURL = "http://daily.cowangroup.ca/EN/Pages/default.aspx" #Uncomment and update for company Intranet site
 $helpdeskURL = "http://phl-helpdesk:8080/WOListView.do"
-$dayStart = "07" #Start of work day -1 hour. Must be in 24h format. Default is 7am.
-$dayEnd = "17" #End of work day. Must be in 24h format. Default is 5pm.
 
 
 # FUNCTIONS
 function launchApps {
-    #Uncomment lines to enable individual apps. I'll be adding popular applications over time.
     Write-Host "Launching Apps..."
-    start-process "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "$helpdeskURL" #Chrome
-    Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE" #Outlook
-    Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\lync.exe" #Skype
-    Start-Process "C:\ProgramData\$env:USERNAME\Microsoft\Teams\current\teams.exe" #Teams
-    #Start-Process "C:\Program Files (x86)\TeamViewer\TeamViewer.exe" #TeamViewer
-    #Start-Process "C:\Program Files (x86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe" #RDP
-    Start-Sleep -Seconds 30
     Start-Process "C:\Program Files (x86)\Telephony\TouchPoint\TouchPoint.exe" #TouchPoint, delayed to fix startup performance.
+    Start-Sleep -Seconds 30
+    start-process "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "$helpdeskURL" #Chrome
+    Start-Sleep -Seconds 10
+    Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE" #Outlook
     Write-Host "Done."
     Start-Sleep -Seconds 10
     Exit
@@ -49,10 +28,4 @@ function launchApps {
 
 
 # SCRIPT
-$time = Get-Date -Format "HH"
-$day = Get-Date -Format "dddd"
-if (($day -ne "Saturday") -or ($day -ne "Sunday")) {
-    if (($time -ge $dayStart) -and ($time -lt $dayEnd)) {
-        launchApps
-    }
-}
+launchApps
