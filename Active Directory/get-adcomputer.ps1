a28283c0e2eebe25141a4fabaadc39de475e0edd
Matt <waldeck.me@gmail.com>
2020-01-30 08:25:38 -0500

    Relocated output
    Relocated the output to the current user's desktop.

--- Active Directory/get-adcomputer.ps1
+++ Active Directory/get-adcomputer.ps1
@@ -4,15 +4,15 @@
     Date:       07-09-2018
     Language:   PowerShell
     Purpose:    Generates a CSV containing all computers in the current domain.
-    Last Edit:  07-09-2019
-    Version:    v1.0.0
+    Last Edit:  30-01-2020
+    Version:    v1.0.1
 #>
 
 # Allow AD commands
 Import-Module ActiveDirectory
 
 # Console feedback
-Write-Output "Generating C:\adcomputers.csv..."
+Write-Output "Generating C:\users\$env:username\adcomputers.csv..."
 
 # Select all computers in the domain and pick out the specified information
 Get-ADComputer -Filter * | Select-Object -Property Name, ObjectClass, Enabled, DistinguishedName |`
