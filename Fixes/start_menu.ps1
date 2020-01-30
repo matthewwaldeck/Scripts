40b093ed4a9745bbe7593e7377f6cc4a1ced27cf
Matt <waldeck.me@gmail.com>
2020-01-30 15:22:02 -0500

    Create start_menu.ps1
    Runs a couple of basic fixes for a broken start menu in Windows 10

---
+++ Fixes/start_menu.ps1
@@ -0,0 +1,27 @@
+<#
+    DESCRIPTION
+    Developer:  Matt Waldeck
+    Date:       01-30-2020
+    Language:   PowerShell
+    Purpose:    Runs throguh basic troubleshooting steps for a broken Start Menu in Windows 10.
+    Last Edit:  01-30-2020
+    Version:    1.0.0
+
+    CHANGELOG:
+    -Initial Build
+
+    TASKS:
+    -Run SFC scan
+    -Reinstall Windows apps
+#>
+
+#Run SFC scan
+Write-Host "Scanning file system for errors..."
+sfc /scannow | Set-Content -Path "C:\Users\$env:USERNAME"
+Write-Host "Scan complete. Results can be found on your desktop."
+
+#Reinstall Windows apps
+$install = Read-Host -Prompt "Would you like to reinstall default apps? (y/n)"
+if ($install -eq "y") {
+    Get-AppXPackage -AllUsers | ForEach-Object {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+}
