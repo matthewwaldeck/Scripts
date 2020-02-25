4cf43ca86e68405c31b0f70b21de1e0debb7d135
Matt <waldeck.me@gmail.com>
2020-02-25 14:27:35 -0500

    Update good_morning.ps1
    Little cleanup.

--- Automation/good_morning.ps1
+++ Automation/good_morning.ps1
@@ -1,22 +1,21 @@
-<# DESCRIPTION
-    Developer:  Matt Waldeck
-    Date:       07-12-2019
-    Language:   PowerShell
-    Purpose:    Launches all my usual apps in the morning. Place shortcut on desktop and run once logged in.
-    Last Edit:  01-24-2020
-    Version:    v3.0.1
+<#
+    DESCRIPTION
+    Author:     Matt Waldeck
+    Created:    12-07-2020
+    Purpose:    Automates the basic setup process for a new Windows PC.
+    Last Edit:  02-25-2020
+    Version:    1.0.0
 #>
 
-
 # VARIABLES & URLS
-#$intranetURL = "http://intranet.company.com/"
-$helpdeskURL = "http://helpdesk.company.com/"
+$url_intranet = "daily.cowangroup.ca/EN/Pages/default.aspx"
+$url_helpdesk = "http://phl-helpdesk:8080/WOListView.do"
 
-
+#Launch programs
 Write-Host "Launching Apps..."
 Start-Process "C:\Program Files (x86)\Telephony\TouchPoint\TouchPoint.exe" #TouchPoint
 Start-Sleep -Seconds 30 #Delayed to give Touchpoint time to load.
-Start-Process "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "$helpdeskURL" #Chrome
+Start-Process "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "$url_intranet, $url_helpdesk" #Chrome
 Start-Sleep -Seconds 10
 Start-Process "C:\Program Files (x86)\Microsoft Office\root\Office16\OUTLOOK.EXE" #Outlook
 Write-Host "Done."
