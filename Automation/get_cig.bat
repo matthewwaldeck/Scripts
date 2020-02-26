0b8a633496090fad834a9d9af8c2d9d330e74137
Matt <waldeck.me@gmail.com>
2020-02-26 13:11:44 -0500

    Update get_cig.bat
    Added some more documentation

--- Automation/get_cig.bat
+++ Automation/get_cig.bat
@@ -2,12 +2,14 @@
 :: Downloads the Cowan TeamViewer app on a user's computer.
 :: Makes a copy on the desktop and renames to "CIG".
 
+:: Download custom TeamViewer client
 ECHO Downloading...
 set download="https://customdesign.teamviewer.com/download/version_15x/enzttzn_windows/TeamViewerQS.exe"
 cd %USERPROFILE%\Downloads
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%download%', 'cowanqs.exe')"
 ECHO Done!
 
+:: Copy downloaded file to desktop and rename to CIG.exe
 :1
 if exist "%USERPROFILE%\Downloads\cowanqs.exe" (
 copy %USERPROFILE%\Downloads\cowanqs.exe %USERPROFILE%\Desktop\CIG.exe
