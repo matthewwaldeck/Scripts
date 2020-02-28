d61910d15b4ef83333bb60316f2455c6c1014469
Matt <waldeck.me@gmail.com>
2020-02-28 15:50:23 -0500

    Setup v0.2.0
    -Added Citrix Workspace install
    -Remade/simplified printer section.
    -Doesn't ask to rename computer every time.
    -Removed complication from app installer.
    -Updated Adobe Reader download link.
    -Housekeeping.
    -Updated some documentation.

--- Automation/setup.ps1
+++ Automation/setup.ps1
@@ -1,11 +1,10 @@
 <#
     DESCRIPTION
-    Developer:  Matt Waldeck
-    Date:       02-25-2020
-    Language:   PowerShell
+    Author:     Matt Waldeck
+    Created:    07-26-2019
     Purpose:    Automates the basic setup process for a new Windows PC.
-    Last Edit:  02-10-2020
-    Version:    0.1.2
+    Last Edit:  02-28-2020
+    Version:    0.2.0
 
     TASKS:
     -Create a log file
@@ -31,149 +30,115 @@ function Install_Programs ($url_download, $program_name) {
     $output = "$env:SystemDrive\temp\$program_name.exe"
     try {
         Write-Host "Downloading $program_name..."
-        "Downloading $program_name..." | Set-Content -Path $logPath
+        "Downloading $program_name..." | Set-Content -Path $path_log
         (New-Object System.Net.WebClient).DownloadFile($url_download, $output)
         Write-Host "Download complete! Running install file..."
-        "Download complete! Running install file..." | Set-Content -Path $logPath
+        "Download complete! Running install file..." | Set-Content -Path $path_log
         Start-Process $output "/S"
         Write-Host "$program_name has been installed successfully!"
-        "$program_name has been installed!" | Set-Content -Path $logPath
+        "$program_name has been installed!" | Set-Content -Path $path_log
     } catch [System.Net.WebException] {
         #Download failed.
         Write-Host "$program_name failed to download!"
-        "$program_name failed to download!" | Set-Content -Path $logPath
-        $_ | Set-Content -Path $logPath
+        "$program_name failed to download!" | Set-Content -Path $path_log
+        $_ | Set-Content -Path $path_log
     } catch {
         #Install failed.
         Write-Host '$program_name failed to install!'
-        "$program_name failed to install!" | Set-Content -Path $logPath
-        $_ | Set-Content -Path $logPath
-    }
-}
-
-function Add_Printer {
-    <# When run, requests the network path to a printer. The function will then attempt to connect to the printer.
-    If it fails, it will save the error message to the log. In future, will also support IP lookup. #>
-    $printer = Read-Host "Enter the full path to the desired printer."
-    try {
-        Add-Printer -ConnectionName $printer
-        Write-Host "$printer has been installed!"
-        "$(Get-TimeStamp) - $printer has been installed." | Add-Content -Path $logPath
-    }
-    catch {
-        Write-Host "Failed to install printer!"
-        "$(Get-TimeStamp) - $printer failed to install. Check the log for details." | Add-Content -Path $logPath
-        $_ | Set-Content -Path $logPath
+        "$program_name failed to install!" | Set-Content -Path $path_log
+        $_ | Set-Content -Path $path_log
     }
 }
 
 
 # VARIABLES #
-$logPath = "C:\Users\$env:UserName\Desktop\setup_$(get-date -f yyyy-MM-dd-HHmm).log"
-$domain = Get-ADDomain -Current LocalComputer
-$ask_printers = ""
-$temp_path = Test-Path -Path "$env:SystemDrive\Users\temp"
 $disks = Get-WmiObject -Class win32_logicaldisk | Where-Object {$PSItem.DriveType -eq 3} | Select-Object Name -ExpandProperty Name
-$array_basic = "Adobe Reader", "Chrome"
-$array_all = "Adobe Reader", "Chrome", "Firefox", "VLC"
+$domain_current = Get-ADDomain -Current LocalComputer
+$domain_new = ""
+$install = ""
+$path_log = "C:\Users\$env:UserName\Desktop\setup_$(get-date -f yyyy-MM-dd-HHmm).log"
+$path_temp = "$env:SystemDrive\temp"
+$path_rename = "$env:SystemDrive\temp\rename.flag"
+$print_ask = ""
+$print_server = ""
+$url_adobe = "https://get.adobe.com/reader/download/?installer=Reader_DC_2020.006.20034_English_for_Windows&os=Windows%2010&browser_type=KHTML&browser_dist=Chrome&dualoffer=false&mdualoffer=true&stype=7491&d=McAfee_Security_Scan_Plus&d=McAfee_Safe_Connect"
 $url_chrome = "https://www.google.com/chrome/thank-you.html?statcb=1&installdataindex=empty&defaultbrowser=0#"
-$url_firefox = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-CA&attribution_code=c291cmNlPXd3dy5nb29nbGUuY29tJm1lZGl1bT1yZWZlcnJhbCZjYW1wYWlnbj0obm90IHNldCkmY29udGVudD0obm90IHNldCkmZXhwZXJpbWVudD0obm90IHNldCkmdmFyaWF0aW9uPShub3Qgc2V0KQ..&attribution_sig=477b9b68ad048e9ad2f25dcfa51f2d4053ae39e1832bd11e63fbd6eb0f4341d6"
-$url_reader = "https://get.adobe.com/reader/download/?installer=Reader_DC_2019.021.20058_English_for_Windows&os=Windows%2010&bowser_type=KHTML&browser_dist=Chrome&dualoffer=false&mdualoffer=true&cr=false&stype=7468&d=McAfee_Security_Scan_Plus&d=McAfee_Safe_Connect"
-$url_vlc = #Need to find VLC download link.
+$url_citrix = "https://www.citrix.com/downloads/workspace-app/windows/workspace-app-for-windows-latest.html#ctx-dl-eula"
 
 
 # SCRIPT #
 #Create logfile
-"$(Get-TimeStamp) - Setup started by $env:UserName" | Set-Content -Path $logPath
+"$(Get-TimeStamp) - Setup started by $env:UserName" | Set-Content -Path $path_log
+
+#Make sure temp folder exists on system drive.
+if (Test-Path -Path $path_temp -eq $false) {
+    New-Item -Path $path_temp -ItemType Directory | Out-Null
+    Write-Host "Created temp directory."
+    "Created temp directory!" | Set-Content -Path $path_log
+} else {
+    Write-Host "Temp directory already present!"
+    "Temp directory already present!" | Set-Content -Path $path_log
+}
 
 #Rename the computer.
-$comp_name =  Read-Host "Enter desired computer name, or press enter to continue."
-if ($comp_name -ne ""){
-    Rename-Computer -NewName $comp_name
-    Write-Host "Computer name set to $comp_name."
-    "$(Get-TimeStamp) - Computer name set to $comp_name." | Add-Content -Path $logPath
+if (Test-Path -Path $path_rename -eq $false) {
+    $comp_name =  Read-Host "Enter desired computer name, or press enter to continue."
+    if ($comp_name -ne ""){
+        Rename-Computer -NewName $comp_name
+        "Computer name has been set!" | Set-Content -Path $path_rename
+        Write-Host "Computer name set to $comp_name."
+        "$(Get-TimeStamp) - Computer name set to $comp_name." | Add-Content -Path $path_log
+    }
 }
 
 #Add the computer to a domain.
-if ($domain.Name -eq ""){
+if ($domain_current.Name -eq ""){
     $join = Read-Host "Would you like to join a domain?"
     if ($join -eq "yes"){
-        $domain_name = Read-Host "What is the domain name?"
+        $domain_new = Read-Host "What is the domain name?"
         try {
-            Add-Computer -DomainName $domain_name
-            "$(Get-TimeStamp) - Successfully joined $domain_name. Rebooting computer..." | Add-Content -Path $logPath
-            "Successfully joined $domain_name. Your computer will now reboot. Please rerun script to continue setup."
+            Add-Computer -DomainName $domain_new
+            "$(Get-TimeStamp) - Successfully joined $domain_new. Rebooting computer..." | Add-Content -Path $path_log
+            "Successfully joined $domain_new. Your computer will now reboot. Please rerun script to continue setup."
             Restart-Computer
         } catch {
-            Write-Host "Failed to join $domain_name"
-            "$(Get-TimeStamp) - Failed to join $domain_name." | Add-Content -Path $logPath
-            $_ | Set-Content -Path $logPath
+            Write-Host "Failed to join $domain_new"
+            "$(Get-TimeStamp) - Failed to join $domain_new." | Add-Content -Path $path_log
+            $_ | Set-Content -Path $path_log
             exit
         }
     }
 }
 
 #Install network printers.
-do {
-    $ask_printers = Read-Host "Would you like to add a network printer?"
-    $ask_printers = $ask_printers.ToLower()
-    if ($ask_printers -eq "yes") {
-        Add_Printer
-    } else {
-        Write-Host "Finished adding printers."
-        "$(Get-TimeStamp) - Finished adding printers." | Add-Content -Path $logPath
-    }
-} until ($ask_printers -eq "no")
-
-#Make sure temp folder exists on system drive.
-if ($temp_path -eq $false) {
-  New-Item -Path "$env:SystemDrive\Users\temp" -ItemType Directory | Out-Null
-  Write-Host "Created temp directory."
-  "Created temp directory!" | Set-Content -Path $logPath
-} else {
-  Write-Host "Temp directory already present!"
-  "Temp directory already present!" | Set-Content -Path $logPath
+$print_ask = Read-Host "Would you like to add a network printer?"
+$print_ask = $print_ask.ToLower()
+if ($print_ask -eq "yes") {
+    $print_server = Read-Host "where is your print server?"
+    Invoke-Item -Path $print_server
 }
 
 #Install some basic programs.
 do {
-    #Write the available app packages to the terminal. Please expand these arrays as software is added.
-    Clear-Host
-    Write-Host "Basic Apps:"
-    foreach ($app in $array_basic) {
-        Write-Host "$app"
-    }
     Write-Host ""
-    Write-Host "All Apps:"
-    foreach ($app in $array_all) {
-        Write-Host "$app"
-    }
-
-    #Ask for a selection of apps to install, or enter "none" to skip to the next task.
-    Write-Host ""
-    $install = Read-Host "Would you like to install a BASIC set of programs, ALL available software, or NONE?"
+    $install = Read-Host "Would you like to install some basic software?"
     $install = $install.ToLower()
-    if ($install -eq 'basic') {
-        install_programs "$url_reader" "Adobe Reader"
+    if ($install -eq 'yes') {
+        install_programs "$url_adobe" "Adobe Reader"
         Install_Programs "$url_chrome" "Chrome"
-    } elseif ($install -eq 'all') {
-        install_programs "$url_reader" "Adobe Reader"
-        Install_Programs "$url_chrome" "Chrome"
-        install_programs "$url_firefox" "Firefox"
-        install_programs "$url_vlc" "VLC"
-    } elseif ($install -eq 'none') {
+        Install_Programs "$url_citrix" "Citrix"
+    } elseif ($install -eq 'no') {
         Write-Host "Nothing has been installed."
-        "No programs were installed." | Add-Content -Path $logPath
+        "No programs were installed." | Add-Content -Path $path_log
     } else {
         Write-Host "Please make a valid selection."
     }
-} while ($install -ne 'basic' -or 'all' -or 'none')
+} until ($install -eq "yes" -or $install -eq "no")
 
-#Delete installer files.
+#Clean up temp folder.
 Remove-Item –path "$env:SystemDrive\temp\Adobe Reader.exe"
 Remove-Item –path "$env:SystemDrive\temp\Chrome.exe"
-Remove-Item –path "$env:SystemDrive\temp\Firefox.exe"
-Remove-Item –path "$env:SystemDrive\temp\VLC.exe"
+Remove-Item -path $path_rename
 
 #Get rid of a few preinstalled ads
 Get-AppxPackage *officehub* | Remove-AppxPackage
@@ -186,23 +151,23 @@ cleanmgr.exe | Out-Null
 
 #Optimize all attached disks.
 Write-Host "Optimizing and Defragging drives..."
-"$(Get-TimeStamp) - Beginning disk optimization..." | Add-Content -Path $logPath
+"$(Get-TimeStamp) - Beginning disk optimization..." | Add-Content -Path $path_log
 ForEach ($disk in $disks) {
-    "$(Get-TimeStamp) - Beginning defrag on $disk." | Add-Content -Path $logPath
+    "$(Get-TimeStamp) - Beginning defrag on $disk." | Add-Content -Path $path_log
     defrag $disk /O | Out-Null
-    "$(Get-TimeStamp) - Completed defrag on $disk." | Add-Content -Path $logPath
+    "$(Get-TimeStamp) - Completed defrag on $disk." | Add-Content -Path $path_log
 }
 Write-Host "Disk optimization complete!"
-"$(Get-TimeStamp) - Disk optimization complete!" | Add-Content -Path $logPath
+"$(Get-TimeStamp) - Disk optimization complete!" | Add-Content -Path $path_log
 
 #Finish up
 Write-Host "Setup complete!"
-"$(Get-TimeStamp) - Setup completed!" | Add-Content -Path $logPath
+"$(Get-TimeStamp) - Setup completed!" | Add-Content -Path $path_log
 $reboot = Read-Host "Would you like to reboot this computer?"
 $reboot = $reboot.ToLower()
 if ($reboot -eq "yes"){
     Write-Host "Restarting your PC..."
-    "$(Get-TimeStamp) - Restarting computer." | Add-Content -Path $logPath
+    "$(Get-TimeStamp) - Restarting computer." | Add-Content -Path $path_log
     Start-Sleep -Seconds 5
     Restart-Computer
 } else {
