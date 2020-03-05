ca514b9638197b8f811b1c23ffe8bbb26d5c0abc
Matt <waldeck.me@gmail.com>
2020-03-05 15:16:07 -0500

    Renamed

--- Active Directory/get-emails.ps1
+++ Active Directory/get-emails.ps1
@@ -3,10 +3,10 @@
     Created:    03-05-2020
     Purpose:    Generate a CSV file of every user's location and email address. Filters out disabled accounts and system accounts.
     Last Edit:  03-05-2020
-    Version:    v01.0.0
+    Version:    v01.1.0
 #>
 
 Write-Host "Saving file to desktop..."
-Get-ADUser -Filter {PasswordNeverExpires -eq $false -and enabled -eq $true} -Properties office, emailaddress |`
+Get-ADUser -Filter {PasswordNeverExpires -eq $false -and Enabled -eq $true} -Properties office, emailaddress |`
 Select-Object Name, office, emailaddress |`
-Sort-Object Name | Export-Csv -Path "$env:USERPROFILE\Desktop\Emails and Locations.csv"
+Sort-Object Name | Export-Csv -Path "$env:USERPROFILE\Desktop\Emails and Locations.csv" -NoTypeInformation
