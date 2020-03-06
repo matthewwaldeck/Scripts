6f753846c8751d744bfecff7d59a67f326f73c6c
Matt <waldeck.me@gmail.com>
2020-03-06 11:45:01 -0500

    get-adusers v1.2.0
    -Now saves CSV to desktop
    -Updated properties
    -Not sorted alphabetically by name.

--- Active Directory/get-users.ps1
+++ Active Directory/get-users.ps1
@@ -1,22 +1,23 @@
 <#
     DESCRIPTION
     Developer:  Matt Waldeck
-    Date:       07-09-2018
+    Date:       07-09-2019
     Language:   PowerShell
     Purpose:    Generates a CSV containing all users in the current domain.
-    Last Edit:  30-01-2020
-    Version:    v1.1.3
+    Last Edit:  03-06-2020
+    Version:    v1.2.0
 #>
 
 # Allow AD commands
 Import-Module ActiveDirectory
 
 # Console feedback
-Write-Output "Generating C:\users\$env:username\adusers.csv..."
+Write-Output "Generating $env:USERPROFILE\Desktop\Domain Users.csv..."
 
 # Select all computers in the domain and pick out the specified information
-Get-ADUser -Filter * | Select-Object -Property Name, ObjectClass, Enabled, UserPrincipalName, DistinguishedName |`
-Export-Csv C:\adusers.csv -NoTypeInformation #Export to root of C:\
+Get-ADUser -Filter * -Properties Name, Enabled, Office, LastLogon, LockedOut, EmailAddress, CanonicalName |`
+Select-Object Name, Enabled, Office, LastLogon, LockedOut, EmailAddress, CanonicalName | Sort-Object Name |`
+Export-Csv "$env:USERPROFILE\Desktop\Domain Users.csv" -NoTypeInformation #CSV saves to desktop.
 
 # Console feedback
 Write-Output "Done!"
