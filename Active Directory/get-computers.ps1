4e7aa2beb0ac5ad1f741363367956ed7568219ed
Matt <waldeck.me@gmail.com>
2020-03-06 11:44:47 -0500

    get-adcomputers v1.1.0
    -Now saves CSV to desktop
    -Updated properties
    -Not sorted alphabetically by name.

--- Active Directory/get-computers.ps1
+++ Active Directory/get-computers.ps1
@@ -1,22 +1,23 @@
 <#
     DESCRIPTION
     Developer:  Matt Waldeck
-    Date:       07-09-2018
+    Date:       07-09-2019
     Language:   PowerShell
     Purpose:    Generates a CSV containing all computers in the current domain.
-    Last Edit:  30-01-2020
-    Version:    v1.0.1
+    Last Edit:  03-06-2020
+    Version:    v1.1.0
 #>
 
 # Allow AD commands
 Import-Module ActiveDirectory
 
 # Console feedback
-Write-Output "Generating C:\users\$env:username\adcomputers.csv..."
+Write-Output "Generating $env:USERPROFILE\Desktop\Domain Computers.csv..."
 
 # Select all computers in the domain and pick out the specified information
-Get-ADComputer -Filter * | Select-Object -Property Name, ObjectClass, Enabled, DistinguishedName |`
-Export-Csv C:\adcomputers.csv -NoTypeInformation #Export to root of C:\
+Get-ADComputer -Filter * -Properties Name, Enabled, OperatingSystem, CanonicalName |`
+Select-Object -Property Name, Enabled, OperatingSystem, CanonicalName | Sort-Object Name |`
+Export-Csv "$env:USERPROFILE\Desktop\Domain Computers.csv" -NoTypeInformation #CSV saves to desktop.
 
 # Console feedback
 Write-Output "Done!"
