014d4824f1f3cd796ddf152422e294bf4c07c64d
Matt <waldeck.me@gmail.com>
2021-05-18 12:05:00 -0400

    Cleanup
    Removing unneeded scripts.

--- Maintenance/server-admin.bat
+++
@@ -1,6 +0,0 @@
-@ECHO off
-
-:: All this file does is run maintenance_server.ps1 with admin permissions. Please do not run maintenance_server.ps1 directly
-:: as it will not function properly, and some features will not work at all.
-
-powershell -noprofile -command "&{ start-process powershell -ArgumentList '-noprofile -file %~dp0maintenance_server.ps1' -verb RunAs}"
