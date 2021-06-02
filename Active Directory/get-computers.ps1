ccd5de8b7c755423ceb20d631d795785c3ac8d85
Matt <waldeck.me@gmail.com>
2021-06-02 11:38:50 -0400

    Relocate output
    Redirected the output path to the Downloads folder and added a 5-second timer to the end of the script.

--- Active Directory/get-computers.ps1
+++ Active Directory/get-computers.ps1
@@ -3,8 +3,8 @@
     Developer:  Matt Waldeck
     Date:       07-09-2019
     Purpose:    Generates a CSV containing all computers in the current domain.
-    Last Edit:  09-11-2020
-    Version:    v1.1.2
+    Last Edit:  06-02-2021
+    Version:    v1.1.3
 #>
 
 # Allow AD commands
@@ -16,8 +16,9 @@ Write-Output "Generating $env:USERPROFILE\Desktop\Domain Computers.csv..."
 # Select all computers in the domain and pick out the specified information
 Get-ADComputer -Filter * -Properties Name, Enabled, OperatingSystem, OperatingSystemVersion, Created, CanonicalName |`
 Select-Object -Property Name, Enabled, OperatingSystem, OperatingSystemVersion, Created, CanonicalName | Sort-Object Name |`
-Export-Csv "$env:USERPROFILE\Desktop\Domain Computers.csv" -NoTypeInformation #CSV saves to desktop.
+Export-Csv "$env:USERPROFILE\Downloads\Domain Computers.csv" -NoTypeInformation #CSV saves to desktop.
 
 # Console feedback
 Write-Output "Done!"
 Write-Output ""
+Start-Sleep -Seconds 5
