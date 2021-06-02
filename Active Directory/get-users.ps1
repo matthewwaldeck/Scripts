7fbe4bc0de22ded4be728d940e9b34132bddf0f7
Matt <waldeck.me@gmail.com>
2021-06-02 11:37:39 -0400

    Filtered to active users
    Added a filter to the Get-ADUser command to limit the results to active accounts, ignoring any that are disabled or have a password that doesn't expire.
    
    Also changed the output to the Downloads folder, and deleted the get-emails.ps1 script as it is no longer needed (functionality rolled into get-users.ps1).

--- Active Directory/get-users.ps1
+++ Active Directory/get-users.ps1
@@ -2,9 +2,9 @@
     DESCRIPTION
     Developer:  Matt Waldeck
     Date:       07-09-2019
-    Purpose:    Generates a CSV containing all users in the current domain.
-    Last Edit:  09-11-2020
-    Version:    v1.2.2
+    Purpose:    Generates a CSV containing all active users in the current domain.
+    Last Edit:  06-02-2021
+    Version:    v1.3.0
 #>
 
 # Allow AD commands
@@ -14,10 +14,11 @@ Import-Module ActiveDirectory
 Write-Output "Generating $env:USERPROFILE\Desktop\Domain Users.csv..."
 
 # Select all computers in the domain and pick out the specified information
-Get-ADUser -Filter * -Properties Name, Enabled, Office, Department, Description, LastLogonDate, LockedOut, EmailAddress, CanonicalName |`
+Get-ADUser -Filter {PasswordNeverExpires -eq $false -and Enabled -eq $true} -Properties Name, Enabled, Office, Department, Description, LastLogonDate, LockedOut, EmailAddress, CanonicalName |`
 Select-Object Name, Enabled, Office, Department, Description, LastLogonDate, LockedOut, EmailAddress, CanonicalName | Sort-Object Name |`
-Export-Csv "$env:USERPROFILE\Desktop\Domain Users.csv" -NoTypeInformation #CSV saves to desktop.
+Export-Csv "$env:USERPROFILE\Downloads\Domain Users.csv" -NoTypeInformation #CSV saves to downloads folder.
 
 # Console feedback
 Write-Output "Done!"
 Write-Output ""
+Start-Sleep -Seconds 5
