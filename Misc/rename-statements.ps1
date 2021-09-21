3fb29e0ddad0088e76ec69384582e0b2ebe4ea54
Matt <8987013+SunglassesSanta@users.noreply.github.com>
2021-09-21 16:28:23 -0400

    Update rename-statements.ps1
    Added console feedback and absolute file path to start.

--- Misc/rename-statements.ps1
+++ Misc/rename-statements.ps1
@@ -3,9 +3,11 @@
     Created:    09-21-2021
     Purpose:    Remove extraneous text from Cowan pay statement filenames.
     Last Edit:  09-21-2021
-    Version:    v1.0.0
+    Version:    v1.1.0
 #>
 
-Set-Location Downloads
+Write-Host "Updating filenames..."
+Set-Location C:\Users\$env:USERNAME\Downloads
 Get-ChildItem -Filter "*current*" -Recurse | Rename-Item -NewName {$_.name -replace 'EmployeePayStatementsDeposits ','' }
 Get-ChildItem | Rename-Item -NewName {$_.name.substring(0,$_.BaseName.length-4) + $_.Extension}
+Write-Host "Done!"
