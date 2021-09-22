cdfee72bca47d044a763c2d290ee434a8f57b96f
Matt <8987013+SunglassesSanta@users.noreply.github.com>
2021-09-22 08:49:54 -0400

    Create disable_hello.ps1
    Once complete, this will disable Windows Hello enrolment via local group policy.

---
+++ Automation/disable_hello.ps1
@@ -0,0 +1,14 @@
+<# DESCRIPTION
+    Author:     Matt Waldeck
+    Created:    07-29-2021
+    Purpose:    Disable Windows Hello enrollment via local Group Policy.
+    Last Edit:  07-09-2021
+    Version:    v0.1.0
+#>
+
+# FUNCTIONS #
+
+#CONSTANTS AND VARIABLES
+
+#MAIN CODE
+Set-GPPrefRegistryValue 
