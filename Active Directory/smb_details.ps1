0eefe85c1428aa6eec8b7d7e299759c2fdbb1818
Matt <8987013+Gediren@users.noreply.github.com>
2022-02-08 09:04:49 -0500

    Create smb_details.ps1
    Created by a friend, searches SMB shares and returns a list of groups and their permissions to the shares.

---
+++ Active Directory/smb_details.ps1
@@ -0,0 +1,84 @@
+Clear-Host
+$SmbServer = "STJOES"
+
+$Cim = New-CimSession -ComputerName $SmbServer
+
+$SmbShares = Get-SmbShare -CimSession $Cim
+
+ 
+
+$Search = Read-Host "Serach"
+
+ 
+
+Write-Host ""
+
+ 
+
+foreach ($SmbShare in $SmbShares) {
+
+   
+
+    if ($SmbShare.Name -imatch $Search) {
+
+        Write-Host "--------------------------------------------"
+
+        $Path = "\\" + $SmbServer + "\"+ $SmbShare.Name
+
+       
+
+        Write-Host "Path:"
+
+        Write-Host $Path -ForegroundColor Red -BackgroundColor Black
+
+        Write-Host ""
+
+ 
+
+        $PermissionsACL = Get-Acl -Path $Path
+
+ 
+
+        foreach ($Identity in $PermissionsACL) {
+
+            foreach ($AccessRight in $Identity.Access) {
+
+               
+
+                $Object = $AccessRight.IdentityReference
+
+                if ($Object -imatch "STJOSEPHS" -or $Object -imatch "HHSC" -and $Object -inotmatch "Domain Admins") {
+
+                   
+
+                    $SamAccountName = $Object.Value.Split('\')[1]
+
+                    $ADObject = Get-ADObject -Filter ('SamAccountName -eq "{0}"' -f $SamAccountName)
+
+       
+
+                    if ($ADObject.ObjectClass -eq 'group') {
+
+                        Write-Host "Group:"
+
+                        Write-Host $Object -ForegroundColor Green -BackgroundColor Black
+
+                        Write-Host ""
+
+                        Write-Host "Permissions:"
+
+                        Write-Host $AccessRight.FileSystemRights -ForegroundColor Blue -BackgroundColor Black
+
+                        Write-Host ""
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
