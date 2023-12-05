fd6cbf0336ac04d6f883293f1d86b36ff9434544
Matt <8987013+Gediren@users.noreply.github.com>
2023-12-05 12:02:28 -0500

    Add files via upload

---
+++ Automation/timesync.ps1
@@ -0,0 +1,40 @@
+﻿<#
+    .NOTES        
+        NAME:    timesync.ps1
+        AUTHOR:  Matt Waldeck
+        VERSION: 1.0.0
+        DATE:    2023/12/05
+        LINK:    https://github.com/Gediren/Windows-Scripts/blob/master/Automation/tiemsync.ps1
+
+        VERSION HISTORY
+            1.0.0 - 2023.12.05
+                Initial Version
+    
+    .SYNOPSIS
+        Update Windows time server.
+
+    .DESCRIPTION
+        This script will update the server Windows system clock is syncing to.
+        It will present the current time server, and ask if the admin would like to update to NTP.
+
+    .INPUTS
+        None. You cannot pipe objects to this script.
+
+    .OUTPUTS
+        None. This script does not generate any output.
+
+    .LINK
+        https://github.com/Gediren/
+#>
+
+
+$current = w32tm /query /source
+Write-Host "The clock is synced to $current."
+
+$update = Read-Host -Prompt "Would you like to update to NTP? (y/n)"
+if ($update -eq 'y') {
+    try {
+        w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org" /update
+        Write-Host "Time has been updated to use the NTP pool."
+    } catch {Write-Host "The update failed. You are still using $current."}
+} else {exit}
