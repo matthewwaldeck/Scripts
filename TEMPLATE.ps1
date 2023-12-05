ce46119d0de153c01822ccfde67b121ede5708b2
Matt <8987013+Gediren@users.noreply.github.com>
2023-12-05 11:03:50 -0500

    Add files via upload

---
+++ TEMPLATE.ps1
@@ -0,0 +1,28 @@
+﻿<#
+    .SYNOPSIS
+        A single line, brief description of the script.
+
+    .DESCRIPTION
+        A longer, more detailed description.
+        Maybe a few lines.
+
+    .INPUTS
+        List any input options here.
+        If none: "None. You cannot pipe objects to this script."
+
+    .OUTPUTS
+        List any output from this script here.
+        If none: "None. This script does not generate any output."
+
+    .NOTES        
+        NAME:    TEMPLATE.ps1
+        AUTHOR:  Matt Waldeck
+        DATE:    2023/12/05
+
+        VERSION HISTORY
+            1.0.0 - 2023.12.05
+                Initial Version
+
+    .LINK
+        https://github.com/Gediren/Windows-Scripts
+#>
