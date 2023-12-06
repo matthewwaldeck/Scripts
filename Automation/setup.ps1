07854cb55f8e8130b1884a617eeb24b312c2fe3f
Matt <8987013+Gediren@users.noreply.github.com>
2023-12-06 16:24:06 -0500

    v0.5.0
    0.5.0 - 2023.12.06
        Updated documentation to use get-help syntax.
        Improved comments and readability.
        Merged console and logging into procedure to cut out repetitive code.
        General usability & readability improvements.

--- Automation/setup.ps1
+++ Automation/setup.ps1
@@ -1,200 +1,246 @@
 <#
-    DESCRIPTION
-    Author:     Matt Waldeck
-    Created:    07-26-2019
-    Purpose:    Automates the basic setup process for a new Windows PC.
-    Version:    0.4.0
+    .NOTES        
+        NAME:    setup.ps1
+        AUTHOR:  Matt Waldeck
+        VERSION: 0.5.0
+        DATE:    2023/12/06
+        LINK:    https://github.com/Gediren/Windows-Scripts/blob/master/Automation/setup.ps1
 
-    TASKS:
-    -Create a log file
-    -Rename the computer
-    -Offer to join a Domain
-    -Offer to add network printers
-    -Offer to install some programs
-    -Clean up bloat and temp files
-    -Offer to reboot computer
+        VERSION HISTORY
+            0.4.0 - 2019.07.26
+                Major cleanup.
+                Working on making the code more ambiguous.
+            0.5.0 - 2023.12.06
+                Updated documentation to use get-help syntax.
+                Improved comments and readability.
+                Merged console and logging into procedure to cut out repetitive code.
+                General usability & readability improvements.
+    
+    .SYNOPSIS
+        Set up a new Windows computer.
+
+    .DESCRIPTION
+        This script will get a lot of the basic setup completed on a new Windows PC.
+        It can install applications, clean up some preinstalled junk, and more.
+        It dumps a log when done for troubleshooting in case of issue.
+        This script remains untested and under development for the time being.
+        Once tested, it will get a full release.
+
+    .INPUTS
+        None. You cannot pipe objects to this script.
+
+    .OUTPUTS
+        A log file on the current user's desktop.
+
+    .LINK
+        https://github.com/Gediren/
 #>
 
 
-# FUNCTIONS #
-function Get-TimeStamp {
-    <# This function simply returns a timestamp of the current date and time.
-    It's used in the logs so I can see if something is taking too long. #>
-    return "[{0:MM/dd/yy} at {0:HH:mm:ss}]" -f (Get-Date)
+
+##### FUNCTIONS #####
+function Log ($comment) {
+    #This function writes output to the console, and writes the same line to the logfile.
+    #It will append the date and time to the beginning of the log entry.
+    $timestamp = "[{0:MM/dd/yy} at {0:HH:mm:ss}]" -f (Get-Date)
+    Write-Host "$comment"
+    "$timestamp - $comment" | Add-Content -Path $path_log
 }
 
-function Install_Programs ($url_download, $program_name) {
+function Install ($url_download, $program_name) {
     #Downloads and installs the selected software package.
+    #Input comes from loop and array down below.
+    
     $output = "$env:SystemDrive\temp\$program_name.exe"
+
     try {
-        Write-Host "Downloading $program_name..."
-        "Downloading $program_name..." | Add-Content -Path $path_log
+        Log "Downloading $program_name..."
         (New-Object System.Net.WebClient).DownloadFile($url_download, $output)
-        Write-Host "Download complete! Running install file..."
-        "Download complete! Installing..." | Add-Content -Path $path_log
+        Log "Download complete! Running install file..."
         Start-Process $output "/S"
-        Write-Host "$program_name has been installed successfully!"
-        "$program_name has been installed!" | Add-Content -Path $path_log
+        Log "$program_name has been installed successfully!"
     } catch [System.Net.WebException] {
         #Download failed.
-        Write-Host "$program_name failed to download!"
-        "$program_name failed to download!" | Add-Content -Path $path_log
-        $_ | Add-Content -Path $path_log
+        Log "$program_name failed to download!"
+        Log $_
     } catch {
         #Installation failed.
-        Write-Host '$program_name failed to install!'
-        "$program_name failed to install!" | Add-Content -Path $path_log
-        $_ | Add-Content -Path $path_log
+        Log "$program_name failed to install!"
+        Log $_
     }
-    Remove-Item -path "$env:SystemDrive\temp\$program_name"
 }
 
 
-# ARRAYS AND VARIABLES #
+
+##### ARRAYS AND VARIABLES #####
+#Constants
+$arr_bloat = @('3dbuilder','getstarted','officehub','skypeapp','solitairecollection','zunemusic','zunevideo')
+$arr_install = @('Reader','Firefox','GitHub', 'Notepad++','Spotify','VSCode','VLC')
 $disks = Get-WmiObject -Class win32_logicaldisk | Where-Object {$PSItem.DriveType -eq 3} | Select-Object Name -ExpandProperty Name
 $domain_current = Get-ADDomain -Current LocalComputer
-$domain_new = ""
-$domain_join = ""
-$install = ""
 $path_log = "C:\Users\$env:UserName\Desktop\setup_$(get-date -f yyyy-MM-dd-HHmm).log"
-$path_temp = "$env:SystemDrive\temp"
 $path_rename = "$env:SystemDrive\temp\rename.flag"
+$path_temp = "$env:SystemDrive\temp"
+
+#Variables
+$domain_join = ""
+$domain_new = ""
+$enterprise = ""
+$install = ""
 $print_ask = ""
 $print_server = ""
-$arr_bloat = @('3dbuilder','getstarted','officehub','skypeapp','solitairecollection','zunemusic','zunevideo')
-$arr_install = @('Reader','Firefox','GitHub','Spotify','VSCode','VLC')
-$url_adobe = "https://get.adobe.com/reader/download/?installer=Reader_DC_2020.006.20034_English_for_Windows&os=Windows%2010&browser_type=KHTML&browser_dist=Chrome&dualoffer=false&mdualoffer=true&stype=7491&d=McAfee_Security_Scan_Plus&d=McAfee_Safe_Connect"
+
+#Download URLs for programs.
+$url_adobe = "https://get.adobe.com/reader/download?os=Windows+11&name=Reader+2023.006.20380+English+for+Windows&lang=en&nativeOs=Windows+10&accepted=&declined=&preInstalled=&site=enterprise"
 $url_firefox = "https://download.mozilla.org/?product=firefox-latest&os=win64&lang=en-US"
 $url_github = "https://central.github.com/deployments/desktop/desktop/latest/win32"
+$url_notepad = "https://notepad-plus-plus.org/downloads/v8.6/"
 $url_spotify = "https://download.scdn.co/SpotifySetup.exe"
 $url_vscode = "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user"
-$url_vlc = "https://get.videolan.org/vlc/3.0.11/win64/vlc-3.0.11-win64.exe"
+$url_vlc = "https://get.videolan.org/vlc/3.0.20/win32/vlc-3.0.20-win32.exe"
 
 
-# SCRIPT #
+
+##### SETUP #####
 #Create log file for troubleshooting.
-"$(Get-TimeStamp) - Setup started by $env:UserName" | Set-Content -Path $path_log
+Log "Setup started by $env:UserName" | Set-Content -Path $path_log
+Write-Host
 
 #Make sure temp folder exists on system drive.
 if (Test-Path -Path $path_temp -eq $false) {
+    #Create new temp folder.
     New-Item -Path $path_temp -ItemType Directory | Out-Null
-    Write-Host "Created temp directory."
-    "Created temp directory!" | Add-Content -Path $path_log
+    Log "Created temp directory."
 } else {
-    Write-Host "Temp directory already present!"
-    "Temp directory already present!" | Add-Content -Path $path_log
+    #Temp folder already exists.
+    Log "Temp directory already present!"
 }
 
 #Rename the computer.
 if (Test-Path -Path $path_rename -eq $false) {
+    Write-Host
     $comp_name =  Read-Host "Enter desired computer name, or press enter to continue."
     if ($comp_name -ne ""){
         Rename-Computer -NewName $comp_name
         "Computer name has been set!" | Add-Content -Path $path_rename
-        Write-Host "Computer name set to $comp_name."
-        "$(Get-TimeStamp) - Computer name set to $comp_name." | Add-Content -Path $path_log
+        Log "Computer name set to $comp_name."
     }
 }
 
-#Add the computer to a domain.
-if ($domain_current.Name -eq ""){
-    $domain_join = Read-Host "Would you like to join a domain?"
-    if ($domain_join.ToLower() -eq "yes"){
-        $domain_new = Read-Host "What is the domain name?"
-        try {
-            Add-Computer -DomainName $domain_new
-            "$(Get-TimeStamp) - Successfully joined $domain_new. Rebooting computer..." | Add-Content -Path $path_log
-            "Successfully joined $domain_new. Your computer will now reboot. Please rerun script to continue setup."
-            Restart-Computer
-        } catch {
-            Write-Host "Failed to join $domain_new"
-            "$(Get-TimeStamp) - Failed to join $domain_new." | Add-Content -Path $path_log
-            $_ | Add-Content -Path $path_log
-            exit
+$enterprise = Read-Host "Enable enterprise setup features? (y/n)"
+if ($enterprise -like "*y*") {
+    #Add the computer to a domain.
+    if ($domain_current.Name -eq ""){
+        Write-Host
+        $domain_join = Read-Host "Would you like to join a domain? (y/n)"
+        if ($domain_join.ToLower() -like "*y*"){
+            $domain_new = Read-Host "What is the domain name?"
+            try {
+                Add-Computer -DomainName $domain_new
+                Log "Successfully joined $domain_new."
+                Log "Your computer will now reboot."
+                Log "Please rerun script after reboot to continue setup."
+                Start-Sleep -Seconds 1
+                Log "Rebooting computer..."
+                Start-Sleep -Seconds 5
+                Restart-Computer
+            } catch {
+                Log "Failed to join $domain_new."
+                Log $_
+                exit
+            }
+        } else {Log "Declined to join domain."}
+    }
+
+    #Install network printers.
+    Write-Host
+    $print_ask = Read-Host "Would you like to add a network printer? (y/n)"
+    $print_ask = $print_ask.ToLower()
+    if ($print_ask -like "*y*") {
+        $print_server = Read-Host "where is your print server?"
+        try{
+            Invoke-Item -Path $print_server
+        } Catch {
+            Log "Failed to add printers!"
         }
-    } else {
-        Write-Host "Declined to join domain."
-        "$(Get-TimeStamp) - Declined to join domain." | Add-Content -Path $path_log
-    }
-}
-
-#Install network printers.
-$print_ask = Read-Host "Would you like to add a network printer?"
-$print_ask = $print_ask.ToLower()
-if ($print_ask -eq "yes") {
-    $print_server = Read-Host "where is your print server?"
-    try{
-        Invoke-Item -Path $print_server
-    } Catch {
-        Write-Host "Failed to add printers!"
-        "$(Get-TimeStamp) - Failed to add printers." | Add-Content -Path $path_log
-    }
-} else {
-    "$(Get-TimeStamp) - No printers added." | Add-Content -Path $path_log
+    } else {Log "No printers added."}
 }
 
 #Installs applications, based on user input. 
 do {
-    Write-Host ""
-    $install = Read-Host "Would you like to install some basic software?"
+    Write-Host
+    $install = Read-Host "Would you like to install any software? (y/n)"
     $install = $install.ToLower()
-    if ($install -eq 'yes') {
+    if ($install -eq "*y*") {
         do {
             Clear-Host
-            foreach ($i in $arr_install) {
-                Write-Host "-$i`n"
+            foreach ($_ in $arr_install) {
+                Write-Host "-$_`n"
             }
-            $app = Read-Host "What would you like to install?"
+            $app = Read-Host "What would you like to install? Write 'done' to finish."
             switch ($app.ToLower()) {
-                reader {install_programs "$url_adobe" "Adobe Reader"}
-                firefox {Install_Programs "$url_firefox" "Firefox"}
-                github {Install_Programs "$url_github" "GitHub Desktop"}
-                spotify {Install_Programs "$url_spotify" "Spotify"}
-                vscode {Install_Programs "$url_vscode" "Visual Studio Code"}
-                vlc {Install_Programs "$url_vlc" "VLC Media Player"}
+                reader {Install "$url_adobe" "Adobe Reader"}
+                firefox {Install "$url_firefox" "Firefox"}
+                github {Install "$url_github" "GitHub Desktop"}
+                notepad {Install "$url_notepad" "Notepad++"}
+                spotify {Install "$url_spotify" "Spotify"}
+                vscode {Install "$url_vscode" "Visual Studio Code"}
+                vlc {Install "$url_vlc" "VLC Media Player"}
                 Default {Write-Host "Please choose from the applications listed, or type done."}
             }
         } until ($app -eq 'done')
     } elseif ($install -eq 'no') {
-        Write-Host "Nothing has been installed."
-        "$(Get-TimeStamp) - No programs were installed." | Add-Content -Path $path_log
+        Log "No programs were installed."
     } else {
-        Write-Host "Please make a valid selection."
+        Log "Please make a valid selection."
     }
 } until ($install -eq "yes" -or $install -eq "no")
 
+
+
+##### CLEANUP #####
 #Remove some preinstalled apps
-"$(Get-TimeStamp) - Beginning cleanup..." | Add-Content -Path $path_log
-foreach ($i in $arr_bloat) {
+Write-Host
+Log "Cleaning up preinstalled bloatware and adware..."
+Start-Sleep -Seconds 2
+
+#Get-AppXPackage | where-object {$_.name -notlike '*store*'} | Remove-AppxPackage
+#Just remove all, except store, and then install those you want.
+#Note that it removes everything, like Cortana, widgets, game bar.
+
+foreach ($_ in $arr_bloat) {
     try {
-        Get-AppxPackage *$i* | Remove-AppxPackage
-        "$(Get-TimeStamp) - $i has been removed." | Add-Content -Path $path_log
+        Get-AppxPackage *$_* | Remove-AppxPackage
+        Log "$_ has been removed."
     } catch {
-        "$(Get-TimeStamp) - Failed to remove $i." | Add-Content -Path $path_log
+        Log "Failed to remove $_."
     }
 }
-Remove-Item -path $path_rename #Remove renaming flag from temp folder.
-cleanmgr.exe | Out-Null #Run Disk Cleanup, wait until closed to continue.
 
 #Optimize all attached disks.
-Write-Host "Beginning disk optimization..."
-"$(Get-TimeStamp) - Beginning disk optimization..." | Add-Content -Path $path_log
-ForEach ($disk in $disks) {
-    "$(Get-TimeStamp) - Beginning defrag on $disk." | Add-Content -Path $path_log
-    defrag $disk /O | Out-Null
-    "$(Get-TimeStamp) - Completed defrag on $disk." | Add-Content -Path $path_log
+Write-Host
+Log "Beginning disk optimization..."
+ForEach ($_ in $disks) {
+    Log "Beginning defrag on $_."
+    defrag $_ /O | Out-Null
+    Log "Completed defrag on $_."
 }
-Write-Host "Disk optimization complete!"
-"$(Get-TimeStamp) - Disk optimization complete!" | Add-Content -Path $path_log
+Log "Disk optimization complete!"
 
-#Finish up
-Write-Host "Setup complete!"
-"$(Get-TimeStamp) - Setup completed!" | Add-Content -Path $path_log
-$reboot = Read-Host "Would you like to reboot this computer?"
+Log "Tidying up..."
+Remove-Item -path "$env:SystemDrive\temp" #Delete temp folder.
+cleanmgr.exe | Out-Null #Run Disk Cleanup, wait until closed to continue.
+Clear-RecycleBin #Empty the Recycle Bin.
+
+
+
+##### FIN #####
+Write-Host
+Log "Setup completed!"
+$reboot = Read-Host "Would you like to reboot this computer? (y/n)"
 $reboot = $reboot.ToLower()
-if ($reboot -eq "yes"){
-    Write-Host "Restarting your PC..."
-    "$(Get-TimeStamp) - Restarting computer." | Add-Content -Path $path_log
+if ($reboot -like "*y*"){
+    Log "Restarting computer."
     Start-Sleep -Seconds 5
     Restart-Computer
 } else {
