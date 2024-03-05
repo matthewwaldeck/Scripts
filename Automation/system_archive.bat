1bdf6fbd83e842f976a2305ec01cf34448c29dc6
Matt <8987013+Gediren@users.noreply.github.com>
2024-03-05 12:11:59 -0500

    system_archive v1.1
    Update 1.1 adds support for Internet Explorer, which is still used in some enterprise environments, as well as making a copy of the shortcuts pinned to the users taskbar.

--- Automation/system_archive.bat
+++ Automation/system_archive.bat
@@ -3,7 +3,7 @@
 :: LAST UPDATE - March 4, 2024
 
 :: NOTES
-:: This script will save user data to a folder on the current user's desktop.
+:: This script will save user data and system details to a temp folder on the C: drive.
 :: This can be used to recover things like bookmarks if data loss occurs.
 :: This script should be run using the user account that is to be backed up.
 
@@ -15,24 +15,40 @@
 set archive=C:\Temp\%COMPUTERNAME%
 set bookmarks_chrome="C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
 set bookmarks_edge="C:\Users\%USERNAME%\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks"
+set bookmarks_ie="C:\Users\%USERNAME%\Favorites"
+set taskbar="C:\Users\waldeck-m\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
 
 :: Create folder structure.
 mkdir %archive%
 mkdir %archive%\Bookmarks\Chrome
 mkdir %archive%\Bookmarks\Edge
+mkdir %archive%\Bookmarks\IE
+mkdir %archive%\TaskBar
 
 :: Grab bookmarks.
 :: Add Firefox support: https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data
 echo Backing up bookmarks...
-copy %bookmarks_chrome% %archive%\Bookmarks\Chrome
-copy %bookmarks_edge% %archive%\Bookmarks\Edge
+xcopy %bookmarks_chrome% %archive%\Bookmarks\Chrome
+xcopy %bookmarks_edge% %archive%\Bookmarks\Edge
+xcopy %bookmarks_ie% %archive%\Bookmarks\IE /S /Y
 echo Done.
 echo.
+
+::Save taskbar pins.
+echo Backing up taskbar pins...
+xcopy %taskbar% %archive%\TaskBar
+echo Done.
+echo.
+
+:: Backup system registry.
+:: Prompt for user input - would they like to back up the registry?
+:: reg export HKLM %archive%\%COMPUTERNAME%.reg \y
 
 :: Save list of installed software.
 echo Recording system information...
 systeminfo > %archive%\systeminfo.txt
-wmic product get name > %archive%\software.txt
+wmic product get name,Version,InstallDate > %archive%\software.txt
 wmic path softwarelicensingservice get OA3xOriginalProductKey > %archive%\windows_key.txt
+net user > %archive%\local_users.txt
 echo Done.
 echo.
